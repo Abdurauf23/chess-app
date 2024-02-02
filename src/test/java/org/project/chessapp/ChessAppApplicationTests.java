cd28782506b759343c0c7f00c5617bdeb715a109
asus <abduraufv23@gmail.com>
2024-02-02 14:16:11 +0500

    initial commit

---
+++ src/test/java/org/project/chessapp/ChessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.chessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
